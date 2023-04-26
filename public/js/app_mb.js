a05193f7dd3cc9a775361683f0891e7917289e40
jeong-ah-lim <59951720+jeong-ah-lim@users.noreply.github.com>
2023-04-27 00:52:37 +0900

    update mb 버전
    swiper 스타일 수정해야함
    html은 swiper 부분만 다름
    css는 클래스명으로 분기
    js는 파일로 분기

---
+++ public/js/app_mb.js
@@ -0,0 +1,34 @@
+(function(){
+    //start - swiper
+    const swiper3 = new Swiper(".mySwiper3", {
+        spaceBetween: 20,
+        slidesPerView: 2,
+        centeredSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }
+    });
+    //end - swiper
+
+    //start - 해시태그 복사하기 
+    const btnClip = document.querySelector('.btnClip');
+    
+    function copyHashTag(text){
+        const textarea = document.createElement('textarea');
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+    }
+
+    function onClip(e){
+        e.preventDefault();
+        const hashTag = document.querySelector('.hashTag').textContent;
+        copyHashTag(hashTag);
+    }
+    btnClip.addEventListener('click', onClip);
+    //end - 해시태그 복사하기
+
+})();
